ca257ab6f4ba5ad9fbcd7daf13b670449479033e
Ashley Grenon <ashley.grenon@gmail.com>
2022-06-12 10:10:53 -0500

    Implemented end game state

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,24 @@
 import options from './options.json' assert { type: "json" };
 
-console.log(options);
-
 /**
  * Main function
  */
 function main() {
     const startButton = document.getElementById('start-button');
     startButton.addEventListener('click', () => {
-        const backgroundAudio = getAudio('assets/soundtrack.wav', loop = true);
+        const backgroundAudio = getAudio('assets/soundtrack.wav', 0.25, true);
         backgroundAudio.play();
 
         const startScene = document.getElementById('start-scene');
         startScene.classList.add('hidden');
+
+        startGame();
+    });
+
+    const restartButton = document.getElementById('restart-button');
+    restartButton.addEventListener('click', () => {
+        const endScene = document.getElementById('end-scene');
+        endScene.classList.add('hidden');
 
         startGame();
     });
@@ -24,6 +30,99 @@ function main() {
 function startGame() {
     const gameScene = document.getElementById('game-scene');
     gameScene.classList.remove('hidden');
+
+    const container = document.getElementById('copy-monster-container');
+    container.classList.remove('hidden');
+
+    const monsterFeatures = generateMonster();
+
+    // hide monster after 3 seconds
+    setTimeout(() => {
+        const container = document.getElementById('copy-monster-container');
+        container.classList.add('hidden');
+
+        const optionsContainer = document.getElementById('options');
+        optionsContainer.classList.remove('hidden');
+
+        let countdown = 10;
+        const countdownSpan = document.getElementById('countdown');
+        countdownSpan.innerText = countdown;
+
+        const interval = setInterval(() => {
+            if(countdown == 0) {
+                clearInterval(interval);
+                endGame();
+            } else {
+                countdown--;
+                countdownSpan.innerText = countdown;
+            }
+        }, 1000);
+    }, 3000);
+}
+
+/**
+ * 
+ */
+function endGame() {
+    const gameScene = document.getElementById('game-scene');
+    gameScene.classList.add('hidden');
+
+    const endScene = document.getElementById('end-scene');
+    endScene.classList.remove('hidden');
+
+    const optionsContainer = document.getElementById('options');
+    optionsContainer.classList.add('hidden');
+
+    const countdownSpan = document.getElementById('countdown');
+    countdownSpan.innerText = 10;
+}
+
+/**
+ * Generates the appearance of a monster to mimic
+ * @returns Monster features
+ */
+function generateMonster() {
+    const leftEyebrow = document.getElementById('copy-left-eyebrow');
+    const rightEyebrow = document.getElementById('copy-right-eyebrow');
+
+    const leftEye = document.getElementById('copy-left-eye');
+    const rightEye = document.getElementById('copy-right-eye');
+
+    const nose = document.getElementById('copy-nose');
+    const mouth = document.getElementById('copy-mouth');
+
+    // clear previous appearance
+    leftEyebrow.className = '';
+    rightEyebrow.className = '';
+    leftEye.className = '';
+    rightEye.className = '';
+    nose.className = '';
+    mouth.className = '';
+
+    // get random features
+    let eyebrowIndex = getRandomInt(0, options.eyebrows.length);
+    let eyeIndex = getRandomInt(0, options.eyes.length);
+    let noseIndex = getRandomInt(0, options.noses.length);
+    let mouthIndex = getRandomInt(0, options.mouth.length);
+    let colorIndex = getRandomInt(0, options.colors.length); 
+
+    let monsterFeatures = {
+        eyebrow: options.eyebrows[eyebrowIndex],
+        eye: options.eyes[eyeIndex],
+        nose: options.noses[noseIndex],
+        mouth: options.mouth[mouthIndex],
+        color: options.colors[colorIndex]
+    }
+
+    // Update monster appearance with new features
+    leftEyebrow.classList.add(monsterFeatures.eyebrow);
+    rightEyebrow.classList.add(monsterFeatures.eyebrow);
+    leftEye.classList.add(monsterFeatures.eye);
+    rightEye.classList.add(monsterFeatures.eye);
+    nose.classList.add(monsterFeatures.nose);
+    mouth.classList.add(monsterFeatures.mouth);
+
+    return monsterFeatures;
 }
 
 /**
@@ -33,7 +132,7 @@ function startGame() {
  * @param {*} loop 
  * @returns 
  */
-function getAudio(url, volume = 0.25, loop = false) {
+function getAudio(url, volume = 0.5, loop = false) {
     const audio = new Audio();
     audio.src = url;
     audio.volume = volume;
